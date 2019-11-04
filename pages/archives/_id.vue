f1edcbe754fd3470ac82ffa347411b8b0e894836
Teeoo <1982890538@qq.com>
2019-11-04 13:56:14 +0800

    评论
    Signed-off-by: Teeoo <1982890538@qq.com>

--- pages/archives/_id.vue
+++ pages/archives/_id.vue
@@ -98,45 +98,45 @@
           >
             <v-subheader>赏赞支持
             </v-subheader>
-            <v-row>
-              <v-col
-                cols="12"
-                md="6"
-                xs="12"
-                sm="12"
-              >
-                <v-row
-                  justify="center"
-                  align="center"
-                >
-                  <v-img
-                    max-height="200"
-                    max-width="200"
-                    src="/img/1572506124831.png"
-                  >
-                    <div class="text-center">微信</div>
-                  </v-img>
-                </v-row>
-              </v-col>
-              <v-col
-                cols="12"
-                md="6"
-                xs="12"
-                sm="12"
-              >
-                <v-row
-                  justify="center"
-                  align="center"
-                >
-                  <v-img
-                    max-height="200"
-                    max-width="200"
-                    src="/img/1572506124832.jpg"
-                  >
-                  </v-img>
-                </v-row>
-              </v-col>
-            </v-row>
+<!--            <v-row>-->
+<!--              <v-col-->
+<!--                cols="12"-->
+<!--                md="6"-->
+<!--                xs="12"-->
+<!--                sm="12"-->
+<!--              >-->
+<!--                <v-row-->
+<!--                  justify="center"-->
+<!--                  align="center"-->
+<!--                >-->
+<!--                  <v-img-->
+<!--                    max-height="200"-->
+<!--                    max-width="200"-->
+<!--                    src="/img/1572506124831.png"-->
+<!--                  >-->
+<!--                    <div class="text-center">微信</div>-->
+<!--                  </v-img>-->
+<!--                </v-row>-->
+<!--              </v-col>-->
+<!--              <v-col-->
+<!--                cols="12"-->
+<!--                md="6"-->
+<!--                xs="12"-->
+<!--                sm="12"-->
+<!--              >-->
+<!--                <v-row-->
+<!--                  justify="center"-->
+<!--                  align="center"-->
+<!--                >-->
+<!--                  <v-img-->
+<!--                    max-height="200"-->
+<!--                    max-width="200"-->
+<!--                    src="/img/1572506124832.jpg"-->
+<!--                  >-->
+<!--                  </v-img>-->
+<!--                </v-row>-->
+<!--              </v-col>-->
+<!--            </v-row>-->
           </v-card>
           <v-card
             flat
@@ -151,18 +151,22 @@
             class="mt-4"
           >
             <v-card-text>
-              <v-form>
+              <v-form ref="form" v-model="valid">
                 <v-row>
                   <v-col cols="12">
                     <v-textarea
                       prepend-icon="textsms"
                       label="快来写下你的骚操作吧"
+                      v-model="text"
+                      :rules="textRules"
                     ></v-textarea>
                   </v-col>
                   <v-col cols="12">
                     <v-text-field
                       prepend-icon="account_circle"
                       label="昵称"
+                      v-model="username"
+                      :rules="nameRules"
                     ></v-text-field>
                   </v-col>
                   <v-col
@@ -174,6 +178,8 @@
                     <v-text-field
                       prepend-icon="email"
                       label="邮箱"
+                      v-model="email"
+                      :rules="emailRules"
                     ></v-text-field>
                   </v-col>
                   <v-col
@@ -186,6 +192,7 @@
                     <v-text-field
                       prepend-icon="link"
                       label="网站（如果有）http(s)://"
+                      v-model="link"
                     ></v-text-field>
                   </v-col>
                 </v-row>
@@ -203,6 +210,7 @@
                 text
                 dark
                 color="pink"
+                @click="push"
               >提交
               </v-btn>
             </v-card-actions>
@@ -211,34 +219,18 @@
             flat
             class="mt-4"
           >
-            <v-list-item>
-              <v-list-item-avatar color="grey"></v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">MisakaTech</v-list-item-title>
-                <v-list-item-subtitle>2016-01-07 02:54</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <v-card-text>
-              Visit ten places on our planet that are undergoing the biggest changes today.
-              <v-card-actions>
-                <v-btn
-                  small
-                  rounded
-                  text
-                  color="deep-purple accent-4"
-                >
-                  Read
-                </v-btn>
-              </v-card-actions>
+            <div v-for="(data,index) in this.ArticleById.comments" :key="index">
               <v-list-item>
-                <v-list-item-avatar color="grey"></v-list-item-avatar>
+                <v-list-item-avatar>
+                  <v-img :src="data.avatar"></v-img>
+                </v-list-item-avatar>
                 <v-list-item-content>
-                  <v-list-item-title class="headline">MisakaTech</v-list-item-title>
-                  <v-list-item-subtitle>2016-01-07 02:54</v-list-item-subtitle>
+                  <v-list-item-title class="headline">{{data.username}}</v-list-item-title>
+                  <v-list-item-subtitle>{{data.createdAt | prettyDate}}</v-list-item-subtitle>
                 </v-list-item-content>
               </v-list-item>
               <v-card-text>
-                Visit ten places on our planet that are undergoing the biggest changes today.
+                {{data.text}}
                 <v-card-actions>
                   <v-btn
                     small
@@ -246,51 +238,11 @@
                     text
                     color="deep-purple accent-4"
                   >
-                    Read
+                    回复
                   </v-btn>
                 </v-card-actions>
               </v-card-text>
-              <v-list-item>
-                <v-list-item-avatar color="grey"></v-list-item-avatar>
-                <v-list-item-content>
-                  <v-list-item-title class="headline">MisakaTech</v-list-item-title>
-                  <v-list-item-subtitle>2016-01-07 02:54</v-list-item-subtitle>
-                </v-list-item-content>
-              </v-list-item>
-              <v-card-text>
-                Visit ten places on our planet that are undergoing the biggest changes today.
-                <v-card-actions>
-                  <v-btn
-                    small
-                    rounded
-                    text
-                    color="deep-purple accent-4"
-                  >
-                    Read
-                  </v-btn>
-                </v-card-actions>
-              </v-card-text>
-            </v-card-text>
-            <v-list-item>
-              <v-list-item-avatar color="grey"></v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">MisakaTech</v-list-item-title>
-                <v-list-item-subtitle>2016-01-07 02:54</v-list-item-subtitle>
-              </v-list-item-content>
-            </v-list-item>
-            <v-card-text>
-              Visit ten places on our planet that are undergoing the biggest changes today.
-              <v-card-actions>
-                <v-btn
-                  small
-                  rounded
-                  text
-                  color="deep-purple accent-4"
-                >
-                  Read
-                </v-btn>
-              </v-card-actions>
-            </v-card-text>
+            </div>
           </v-card>
         </div>
       </v-article-details>
@@ -299,44 +251,45 @@
 </template>
 
 <script>
-import '@/assets/markdown.styl'
-import gql from 'graphql-tag'
-export default {
-  head() {
-    return {
-      title: this.$store.state.title,
-      meta: [
-        {
-          hid: 'description',
-          name: 'description',
-          content: '生如夏花之绚烂，死如秋叶之静美'
-        }
-      ]
-    }
-  },
-  components: {
-    VArticleDetails: {
-      functional: true,
-      render(h, { data, props, children }) {
-        return h(
-          'v-skeleton-loader',
-          {
-            ...data,
-            props: {
-              boilerplate: false,
-              ...props
-            }
-          },
-          children
-        )
-      }
-    }
-  },
-  apollo: {
-    ArticleById() {
+  import '@/assets/markdown.styl'
+  import gql from 'graphql-tag'
+
+  export default {
+    head() {
       return {
-        prefetch: true,
-        query: gql`
+        title: this.$store.state.title,
+        meta: [
+          {
+            hid: 'description',
+            name: 'description',
+            content: '生如夏花之绚烂，死如秋叶之静美'
+          }
+        ]
+      }
+    },
+    components: {
+      VArticleDetails: {
+        functional: true,
+        render(h, { data, props, children }) {
+          return h(
+            'v-skeleton-loader',
+            {
+              ...data,
+              props: {
+                boilerplate: false,
+                ...props
+              }
+            },
+            children
+          )
+        }
+      }
+    },
+    apollo: {
+      ArticleById() {
+        return {
+          prefetch: true,
+          query: gql`
           query($id: String!) {
             ArticleById(id: $id) {
               id
@@ -366,60 +319,136 @@ export default {
                 id
                 label
               }
+              comments{
+               id
+               order
+               desc
+               createdAt
+               updatedAt
+               ip
+               url
+               email
+               avatar
+               username
+               agent
+               text
+               children
+               parent
+              }
             }
           }
         `,
-        variables() {
-          return {
-            id: String(this.$route.params.id)
-          }
-        },
-        watchLoading(isLoading, countModifier) {
-          // console.info(isLoading, countModifier)
-        },
-        result({ data, loading, networkStatus }) {
-          // console.log({ data, loading, networkStatus })
-          if (this.ArticleById && networkStatus === 7) {
-            const toc = this.ArticleById.toc
-              ? { IsToc: true, data: this.ArticleById.toc }
-              : {
+          variables() {
+            return {
+              id: String(this.$route.params.id)
+            }
+          },
+          watchLoading(isLoading, countModifier) {
+            // console.info(isLoading, countModifier)
+          },
+          result({ data, loading, networkStatus }) {
+            // console.log({ data, loading, networkStatus })
+            if (this.ArticleById && networkStatus === 7) {
+              const toc = this.ArticleById.toc
+                ? { IsToc: true, data: this.ArticleById.toc }
+                : {
                   IsToc: false,
                   data: {}
                 }
-            this.$store.commit('toc/add', toc)
-            this.$store.commit('setTitle', { title: this.ArticleById.title })
+              this.$store.commit('toc/add', toc)
+              this.$store.commit('setTitle', { title: this.ArticleById.title })
+            }
           }
         }
       }
-    }
-  },
-  data() {
-    return {
-      url: ``,
-      links: false,
-      imageUrl: require('../../static/icon.png')
-    }
-  },
-  watch: {},
-  methods: {
-    qrcode(text) {
-      let QRCode = null
-      if (process.browser) {
-        QRCode = require('qrcodejs2')
-        //  TODO:
+    },
+    data() {
+      return {
+        valid: true,
+        username: '',
+        nameRules: [
+          v => !!v || '昵称不能为空!',
+          v => v.length <= 10 || '昵称不能太长了哦!'
+        ],
+        email: '',
+        emailRules: [
+          v => !!v || '邮箱必须填写!',
+          v => /.+@.+/.test(v) || '邮箱格式不正确!'
+        ],
+        text: '',
+        textRules: [
+          v => !!v || '评论内容不能为空!'
+        ],
+        link: '',
+        parent: undefined,
+        url: '',
+        links: false,
+        imageUrl: require('../../static/icon.png')
       }
+    },
+    watch: {},
+    methods: {
+      qrcode(text) {
+        let QRCode = null
+        if (process.browser) {
+          QRCode = require('qrcodejs2')
+          //  TODO:
+        }
+      },
+      async push() {
+        if (this.$refs.form.validate()) {
+          try {
+            const result = await this.$apollo.mutate(
+              {
+                mutation: gql`
+              mutation($data:PushCommentsInput!){
+                PushComments(data: $data) {
+                        id
+                        order
+                        desc
+                        createdAt
+                        updatedAt
+                        ip
+                        url
+                        avatar
+                        email
+                        username
+                        agent
+                        text
+                        children
+                        parent
+                   }
+              }
+              `,
+                variables: {
+                  data: {
+                    url: this.link,
+                    email: this.email,
+                    username: this.username,
+                    text: this.text,
+                    article: this.ArticleById.id,
+                    parent: undefined
+                  }
+                }
+              }
+            )
+            this.ArticleById.comments.push(result.data.PushComments)
+          } catch (e) {
+            console.info(e)
+          }
+        }
+      }
+    },
+    created() {
+      if (process.browser) {
+        this.url = `${window.location.href}`
+      }
+      this.$store.commit('toggle', { qrcode: true, qrcodeUrl: this.url })
+    },
+    destroyed() {
+      this.$store.commit('toc/add', { IsToc: false, data: {} })
+      this.$store.commit('toggle', { qrcode: false, qrcodeUrl: '' })
+      this.$store.commit('setTitle', { title: 'Mr. Lee\'s Blog' })
     }
-  },
-  created() {
-    if (process.browser) {
-      this.url = `${window.location.href}`
-    }
-    this.$store.commit('toggle', { qrcode: true, qrcodeUrl: this.url })
-  },
-  destroyed() {
-    this.$store.commit('toc/add', { IsToc: false, data: {} })
-    this.$store.commit('toggle', { qrcode: false, qrcodeUrl: '' })
-    this.$store.commit('setTitle', { title: "Mr. Lee's Blog" })
   }
-}
 </script>
